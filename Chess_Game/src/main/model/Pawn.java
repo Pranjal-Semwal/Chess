ce4e4632c7a95d18e80363f4a1a6c865c36c1154
Pranjal Semwal <pranjalsemwal143@gmail.com>
2026-08-28 18:49:47 +0530

    Refactor Pawn movement logic to include board state

--- Chess_Game/src/main/model/Pawn.java
+++ Chess_Game/src/main/model/Pawn.java
@@ -4,17 +4,32 @@ public class Pawn extends Piece{
     public Pawn(String color){
         super(color);
     }
-    public boolean isValidMove(int startRow, int startCol, int endRow, int endCol) {
-        int direction=color.equals("WHITE")?-1:1;
-        if(startCol==endCol && endRow==startRow+direction){
+    @Override
+    public boolean isValidMove(int sr, int sc, int er, int ec, Piece[][] board) {
+
+        int direction = color.equals("WHITE") ? -1 : 1;
+
+        // Forward move (1 step)
+        if (sc == ec && er == sr + direction && board[er][ec] == null) {
             return true;
         }
-        if(startCol==endCol && ((color.equals("WHITE") && startRow==6) || (color.equals("BLACK") && startRow==1))){
+
+        // First move (2 steps)
+        if (sc == ec &&
+                ((color.equals("WHITE") && sr == 6) || (color.equals("BLACK") && sr == 1)) &&
+                er == sr + 2 * direction &&
+                board[sr + direction][sc] == null &&
+                board[er][ec] == null) {
             return true;
         }
-        if(Math.abs(startCol-endCol)==1 && endRow==startRow+direction){
+
+        // Diagonal capture
+        if (Math.abs(sc - ec) == 1 && er == sr + direction &&
+                board[er][ec] != null &&
+                !board[er][ec].getColor().equals(color)) {
             return true;
         }
+
         return false;
     }
 }
