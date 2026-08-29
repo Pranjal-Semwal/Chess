30831d57018e255482c5890f844640a895a9e6bc
Pranjal Semwal <pranjalsemwal143@gmail.com>
2026-08-29 09:43:14 +0530

    Enhance Bishop class with valid move logic
    Implement Bishop's movement logic to validate diagonal moves.

--- Chess_Game/src/main/model/Bishop.java
+++ Chess_Game/src/main/model/Bishop.java
@@ -5,7 +5,30 @@ public class Bishop extends Piece{
         super(color);
     }
     @Override
-    public boolean isValidMove(int startRow, int startCol, int endRow, int endCol, Piece[][] board) {
+    public boolean isValidMove(int sr, int sc, int er, int ec, Piece[][] board) {
+        if (sr == er && sc == ec) {
+            return false;
+        }
+
+        int rowDiff = Math.abs(er - sr);
+        int colDiff = Math.abs(ec - sc);
+        if(rowDiff!=colDiff) return false;
+
+        int rowStep = Integer.compare(er, sr);
+        int colStep = Integer.compare(ec, sc);
+
+        int row = sr + rowStep;
+        int col = sc + colStep;
+
+        while (row != er || col != ec) {
+
+            if (board[row][col] != null) {
+                return false;
+            }
+
+            row += rowStep;
+            col += colStep;
+        }
         return true;
     }
 }
